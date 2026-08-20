b930e580caca97724263f790d573ef0c40866ad0
akibasho6-alt <akibasho6@gmail.com>
2026-08-20 21:44:25 +0900

    Game update

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,13 +2,19 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const fs = require('fs');
+
+const sourceModuleScript = /\s*<script\s+type=["']module["']\s+src=["']js\/app\.js["']><\/script>/;
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
-      inject: false,
+      // Keep index.html usable by the local ES-module server, but remove its
+      // source entry from the production copy and inject webpack's bundle.
+      templateContent: () => fs.readFileSync('./index.html', 'utf8').replace(sourceModuleScript, ''),
+      inject: 'body',
+      scriptLoading: 'defer',
     }),
     new CopyPlugin({
       patterns: [
